06f091b0170085cd361310bb29f2b88a0efbed69
ameya.natu@clearroute.io <ameya.natu@clearroute.io>
2024-04-09 17:20:32 +0530

    Initial Commit

---
+++ sample_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sample_app
+//
+//  Created by ameya.natu@clearroute.io on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sample_app/sample_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_appApp.swift
+//  sample_app
+//
+//  Created by ameya.natu@clearroute.io on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct sample_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
